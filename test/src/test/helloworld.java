89bf79442f75f73aa1f5774c6a9c58db97ee9020
HicirTech <luozeting97@gmail.com>
2018-03-10 12:48:45 +1300

    "if pushed, successed!"

--- test/src/test/helloworld.java
+++ test/src/test/helloworld.java
@@ -3,6 +3,8 @@ package test;
 public class helloworld {
 	public static void main(String args[])
 	{
+		System.out.println("test1");
 		System.out.println("Hello World");//how to use???
+		System.out.println("Working Testing success!");;
 	}
 }
