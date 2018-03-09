0669a25824fb89bcc6ee375ff3f5e2f885d5d7f4
HicirTech <luozeting97@gmail.com>
2018-03-10 12:39:18 +1300

    "updating"

--- test/src/test/helloworld.java
+++ test/src/test/helloworld.java
@@ -3,6 +3,6 @@ package test;
 public class helloworld {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World");//how to use?
+		System.out.println("Hello World");//how to use???
 	}
 }
