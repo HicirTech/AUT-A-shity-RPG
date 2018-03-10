eb26088bc4ba3db0b9b616d2281737ea648b5f6e
HicirTech <luozeting97@gmail.com>
2018-03-10 13:08:45 +1300

    hello world保留后删除其余信息 添加交流区文件

--- test/src/test/helloworld.java
+++ test/src/test/helloworld.java
@@ -3,8 +3,6 @@ package test;
 public class helloworld {
 	public static void main(String args[])
 	{
-		System.out.println("test1");
-		System.out.println("Hello World");//how to use???
-		System.out.println("Working Testing success!");;
+		System.out.println("Hello World");
 	}
 }
